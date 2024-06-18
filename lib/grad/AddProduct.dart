925088e43def3d8c29f11e269d026e9ed74609f1
mennatallahsoliman <134705696+mennatallahsoliman@users.noreply.github.com>
2024-06-18 21:44:54 +0300

    Update AddProduct.dart

--- lib/grad/AddProduct.dart
+++ lib/grad/AddProduct.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
-
-void main() {
-  runApp(MaterialApp(
-    home: AddProductScreen(),
-  ));
-}
+import 'product.dart';
 
 class AddProductScreen extends StatefulWidget {
+  final Product? product;
+
+  const AddProductScreen({Key? key, this.product}) : super(key: key);
+
   @override
   _AddProductScreenState createState() => _AddProductScreenState();
 }
 
 class _AddProductScreenState extends State<AddProductScreen> {
   List<XFile> _productImages = [];
+  TextEditingController _productNameController = TextEditingController();
+  TextEditingController _productDescriptionController = TextEditingController();
+  TextEditingController _productPriceController = TextEditingController();
+  TextEditingController _productStockController = TextEditingController();
 
   // Mock data for category and subcategory
   List<String> categories = [
@@ -35,6 +38,20 @@ class _AddProductScreenState extends State<AddProductScreen> {
 
   String selectedCategory = "";
   String selectedSubcategory = "";
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.product != null) {
+      _productNameController.text = widget.product!.name;
+      _productDescriptionController.text = widget.product!.description;
+      _productPriceController.text = widget.product!.price.toString();
+      _productStockController.text = widget.product!.stockStatus;
+      selectedCategory = widget.product!.category;
+      selectedSubcategory = widget.product!.subcategory;
+      _productImages.add(XFile(widget.product!.imageFile.path));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -59,8 +76,31 @@ class _AddProductScreenState extends State<AddProductScreen> {
                   color: const Color(0xFF684399).withOpacity(0.2),
                   borderRadius: BorderRadius.circular(8.0),
                 ),
-                child: const Center(
-                  child: Text(
+                child: Center(
+                  child: _productImages.isNotEmpty
+                      ? Stack(
+                    children: [
+                      Image.file(
+                        File(_productImages.first.path),
+                        height: double.infinity,
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                      ),
+                      Positioned(
+                        top: 0,
+                        right: 0,
+                        child: IconButton(
+                          icon: const Icon(Icons.close, color: Colors.white),
+                          onPressed: () {
+                            setState(() {
+                              _productImages.clear();
+                            });
+                          },
+                        ),
+                      ),
+                    ],
+                  )
+                      : const Text(
                     'Upload Product Image',
                     style: TextStyle(
                       color: Color(0xFF684399),
@@ -79,16 +119,18 @@ class _AddProductScreenState extends State<AddProductScreen> {
             ),
             const SizedBox(height: 10),
             TextFormField(
+              controller: _productNameController,
               decoration: const InputDecoration(labelText: 'Product Title'),
             ),
             const SizedBox(height: 10),
             TextFormField(
-              decoration:
-                  const InputDecoration(labelText: 'Product Description'),
+              controller: _productDescriptionController,
+              decoration: const InputDecoration(labelText: 'Product Description'),
               maxLines: 3,
             ),
             const SizedBox(height: 10),
             TextFormField(
+              controller: _productPriceController,
               decoration: const InputDecoration(labelText: 'Price'),
               keyboardType: TextInputType.number,
             ),
@@ -99,35 +141,30 @@ class _AddProductScreenState extends State<AddProductScreen> {
             ),
             const SizedBox(height: 10),
             TextFormField(
-              decoration:
-                  const InputDecoration(labelText: 'Available Quantity'),
+              controller: _productStockController,
+              decoration: const InputDecoration(labelText: 'Available Quantity'),
               keyboardType: TextInputType.number,
             ),
             const SizedBox(height: 20),
             buildDropdown("Category", categories, (value) {
               setState(() {
-                selectedCategory = value ?? "";
-                selectedSubcategory =
-                    ""; // Reset subcategory when category changes
+                selectedCategory = value ?? selectedCategory;
+                selectedSubcategory = ""; // Reset subcategory when category changes
               });
             }),
             const SizedBox(height: 10),
-            buildDropdown("Subcategory", subcategories[selectedCategory] ?? [],
-                (value) {
+            buildDropdown("Subcategory", subcategories[selectedCategory] ?? [], (value) {
               setState(() {
-                selectedSubcategory = value ?? "";
+                selectedSubcategory = value ?? selectedSubcategory;
               });
             }),
             const SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {
-                // Implement functionality to save the product
-              },
+              onPressed: _canSaveProduct() ? _saveProduct : null,
               style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xFF684399),
+                backgroundColor: _canSaveProduct() ? const Color(0xFF684399) : Colors.grey,
               ),
-              child: const Text('Save Product',
-                  style: TextStyle(color: Colors.white)),
+              child: const Text('Save Product', style: TextStyle(color: Colors.white)),
             ),
           ],
         ),
@@ -137,7 +174,7 @@ class _AddProductScreenState extends State<AddProductScreen> {
 
   void _uploadProductImages(BuildContext context) async {
     final pickedImages = await ImagePicker().pickMultiImage();
-    if (pickedImages != null && pickedImages.isNotEmpty) {
+    if (pickedImages.isNotEmpty) {
       setState(() {
         _productImages = pickedImages;
       });
@@ -168,12 +205,11 @@ class _AddProductScreenState extends State<AddProductScreen> {
     );
   }
 
-  Widget buildDropdown(
-      String labelText, List<String> items, void Function(String?)? onChanged) {
+  Widget buildDropdown(String labelText, List<String> items, void Function(String?)? onChanged) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 20),
       child: DropdownButtonFormField<String>(
-        value: items.isEmpty ? null : items.first,
+        value: items.contains(selectedCategory) ? selectedCategory : null,
         onChanged: onChanged,
         items: items.map((String item) {
           return DropdownMenuItem<String>(
@@ -187,6 +223,31 @@ class _AddProductScreenState extends State<AddProductScreen> {
           floatingLabelBehavior: FloatingLabelBehavior.always,
           border: const OutlineInputBorder(),
         ),
+      ),
+    );
+  }
+
+  bool _canSaveProduct() {
+    return _productNameController.text.isNotEmpty &&
+        _productDescriptionController.text.isNotEmpty &&
+        _productPriceController.text.isNotEmpty &&
+        _productStockController.text.isNotEmpty &&
+        selectedCategory.isNotEmpty &&
+        selectedSubcategory.isNotEmpty &&
+        _productImages.isNotEmpty;
+  }
+
+  void _saveProduct() {
+    Navigator.pop(
+      context,
+      Product(
+        name: _productNameController.text,
+        description: _productDescriptionController.text,
+        price: double.parse(_productPriceController.text),
+        stockStatus: _productStockController.text,
+        category: selectedCategory,
+        subcategory: selectedSubcategory,
+        imageFile: File(_productImages.first.path),
       ),
     );
   }
